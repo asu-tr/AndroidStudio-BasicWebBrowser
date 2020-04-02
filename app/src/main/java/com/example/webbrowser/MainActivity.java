490a65a4367d4448f1708e1e6c93e19bd46bd83b
Asu <29000874+asu-tr@users.noreply.github.com>
2020-04-02 17:25:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.webbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        webView = findViewById(R.id.webView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isNetworkAvailable() == true){
+
+
+                    try {
+                        String weburl = editText.getText().toString();
+                        webView.setWebViewClient(new WebViewClient());
+
+                        webView.loadUrl(weburl);
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+
+                else{
+                    Toast.makeText(MainActivity.this, "Baglantinizi kontrol edin", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+    }
+
+
+
+
+    private boolean isNetworkAvailable(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+}
